ba764da3309a1240575d2a17ab7ef4ef665818aa
NagiMS <2457963787@qq.com>
2023-10-15 20:30:45 +0800

    Site updated: 2023-10-15 20:30:42

---
+++ js/random_pic.js
@@ -0,0 +1,149 @@
+function random_pic(){
+    var random_int= Math.floor(Math.random()*60)+1
+    return random_int
+}
+
+function random_get(random_int){
+    if (random_int==1 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_110_1.jpg"}
+    if (random_int==2 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_12_1.jpg"}
+    if (random_int==3 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_13_1.jpg"}
+    if (random_int==4 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_148_1.jpg"}
+    if (random_int==5 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_157_1.jpg"}
+    if (random_int==6 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_162_1.jpg"}
+    if (random_int==7 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_167_1.jpg"}
+    if (random_int==8 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_179_1.jpg"}
+    if (random_int==9 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_216_1.jpg"}
+    if (random_int==10 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_221_1.jpg"}
+    if (random_int==11 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_226_1.jpg"}
+    if (random_int==12 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_239_1.jpg"}
+    if (random_int==13 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_241_1.jpg"}
+    if (random_int==14 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_245_1.jpg"}
+    if (random_int==15 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_251_1.jpg"}
+    if (random_int==16 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_30_1.jpg"}
+    if (random_int==17 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_39_1.jpg"}
+    if (random_int==18 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_46_1.jpg"}
+    if (random_int==19 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_51_1.jpg"}
+    if (random_int==20 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_56_1.jpg"}
+    if (random_int==21 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_5_1.jpg"}
+    if (random_int==22 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_63_1.jpg"}
+    if (random_int==23 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_65_1.jpg"}
+    if (random_int==24 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_73_1.jpg"}
+    if (random_int==25 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_74_1.jpg"}
+    if (random_int==26 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_76_1.jpg"}
+    if (random_int==27 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_80_1.jpg"}
+    if (random_int==28 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_91_1.jpg"}
+    if (random_int==29 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_98_1.jpg"}
+    if (random_int==30 ){
+    var pic ="http://jkysei.space/image/10.14/1011D_9_1.jpg"}
+
+    if (random_int==31 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_106_1.jpg"}
+    if (random_int==32 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_107_1.jpg"}
+    if (random_int==33 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_114_1.jpg"}
+    if (random_int==34 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_117_1.jpg"}
+    if (random_int==35 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_11_1.jpg"}
+    if (random_int==36 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_126_1.jpg"}
+    if (random_int==37 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_144_1.jpg"}
+    if (random_int==38 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_151_1.jpg"}
+    if (random_int==39 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_153_1.jpg"}
+    if (random_int==40 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_155_1.jpg"}
+    if (random_int==41 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_162_1.jpg"}
+    if (random_int==42 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_167_1.jpg"}
+    if (random_int==43 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_173_2.jpg"}
+    if (random_int==44 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_1_17.jpg"}
+    if (random_int==45 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_1_26.jpg"}
+    if (random_int==46 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_218_1.jpg"}
+    if (random_int==47 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_228_1.jpg"}
+    if (random_int==48 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_237_1.jpg"}
+    if (random_int==49 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_258_1.jpg"}
+    if (random_int==50 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_275_1.jpg"}
+    if (random_int==51 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_277_1.jpg"}
+    if (random_int==52 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_27_1.jpg"}
+    if (random_int==53 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_285_1.jpg"}
+    if (random_int==54 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_30_1.jpg"}
+    if (random_int==55 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_51_2.jpg"}
+    if (random_int==56 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_56_1.jpg"}
+    if (random_int==57 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_60_5.jpg"}
+    if (random_int==58 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_66_1.jpg"}
+    if (random_int==59 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_68_1.jpg"}
+    if (random_int==60 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_75_1.jpg"}
+    if (random_int==61 ){
+    var pic ="http://jkysei.space/image/10.11/1010D_93_1.jpg"}
+    return pic 
+}
+
+function main_random(){
+    var img = $(".random_img")
+    var random_int=random_pic()
+    var pic = random_get(random_int)
+    img.attr("src",pic)
+    new_text(random_int)
+}
+function new_text(random_int){
+    var text_div= $(".random_img_click_block")
+    if (text_div.text()==""){
+        text_div.after("<p>点我继续生成，当前随机数为"+String(random_int)+"</p>")
+    }
+    else{
+        $(".random_img_click_block>p").text("点我继续生成，当前随机数为"+String(random_int))
+    }
+
+}
