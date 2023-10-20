24931b350bf6761785a9c0c693baf0f8d06143d6
NagiMS <2457963787@qq.com>
2023-10-20 15:15:33 +0800

    Site updated: 2023-10-20 15:15:29

--- js/random_pic.js
+++ js/random_pic.js
@@ -2,131 +2,16 @@ function random_pic(){
     var random_int= Math.floor(Math.random()*60)+1
     return random_int
 }
+picurl1 ='http://jkysei.space/image/10.14/1011D_110_1.jpg';picurl2 ='http://jkysei.space/image/10.14/1011D_12_1.jpg';picurl3 ='http://jkysei.space/image/10.14/1011D_13_1.jpg';picurl4 ='http://jkysei.space/image/10.14/1011D_148_1.jpg';picurl5 ='http://jkysei.space/image/10.14/1011D_157_1.jpg';picurl6 ='http://jkysei.space/image/10.14/1011D_162_1.jpg';picurl7 ='http://jkysei.space/image/10.14/1011D_167_1.jpg';picurl8 ='http://jkysei.space/image/10.14/1011D_179_1.jpg';picurl9 ='http://jkysei.space/image/10.14/1011D_216_1.jpg';picurl10 ='http://jkysei.space/image/10.14/1011D_221_1.jpg';picurl11 ='http://jkysei.space/image/10.14/1011D_226_1.jpg';picurl12 ='http://jkysei.space/image/10.14/1011D_239_1.jpg';picurl13 ='http://jkysei.space/image/10.14/1011D_241_1.jpg';picurl14 ='http://jkysei.space/image/10.14/1011D_245_1.jpg';picurl15 ='http://jkysei.space/image/10.14/1011D_251_1.jpg';picurl16 ='http://jkysei.space/image/10.14/1011D_30_1.jpg';picurl17 ='http://jkysei.space/image/10.14/1011D_39_1.jpg';picurl18 ='http://jkysei.space/image/10.14/1011D_46_1.jpg';picurl19 ='http://jkysei.space/image/10.14/1011D_51_1.jpg';picurl20 ='http://jkysei.space/image/10.14/1011D_56_1.jpg';picurl21 ='http://jkysei.space/image/10.14/1011D_5_1.jpg';picurl22 ='http://jkysei.space/image/10.14/1011D_63_1.jpg';picurl23 ='http://jkysei.space/image/10.14/1011D_65_1.jpg';picurl24 ='http://jkysei.space/image/10.14/1011D_73_1.jpg';picurl25 ='http://jkysei.space/image/10.14/1011D_74_1.jpg';picurl26 ='http://jkysei.space/image/10.14/1011D_76_1.jpg';picurl27 ='http://jkysei.space/image/10.14/1011D_80_1.jpg';picurl28 ='http://jkysei.space/image/10.14/1011D_91_1.jpg';picurl29 ='http://jkysei.space/image/10.14/1011D_98_1.jpg';picurl30 ='http://jkysei.space/image/10.14/1011D_9_1.jpg';
+
+picurl31 ='http://jkysei.space/image/10.11/1010D_106_1.jpg';picurl32 ='http://jkysei.space/image/10.11/1010D_107_1.jpg';picurl33 ='http://jkysei.space/image/10.11/1010D_114_1.jpg';picurl34 ='http://jkysei.space/image/10.11/1010D_117_1.jpg';picurl35 ='http://jkysei.space/image/10.11/1010D_11_1.jpg';picurl36 ='http://jkysei.space/image/10.11/1010D_126_1.jpg';picurl37 ='http://jkysei.space/image/10.11/1010D_144_1.jpg';picurl38 ='http://jkysei.space/image/10.11/1010D_151_1.jpg';picurl39 ='http://jkysei.space/image/10.11/1010D_153_1.jpg';picurl40 ='http://jkysei.space/image/10.11/1010D_155_1.jpg';picurl41 ='http://jkysei.space/image/10.11/1010D_162_1.jpg';picurl42 ='http://jkysei.space/image/10.11/1010D_167_1.jpg';picurl43 ='http://jkysei.space/image/10.11/1010D_173_2.jpg';picurl44 ='http://jkysei.space/image/10.11/1010D_1_17.jpg';picurl45 ='http://jkysei.space/image/10.11/1010D_1_26.jpg';picurl46 ='http://jkysei.space/image/10.11/1010D_218_1.jpg';picurl47 ='http://jkysei.space/image/10.11/1010D_228_1.jpg';picurl48 ='http://jkysei.space/image/10.11/1010D_237_1.jpg';picurl49 ='http://jkysei.space/image/10.11/1010D_258_1.jpg';picurl50 ='http://jkysei.space/image/10.11/1010D_275_1.jpg';picurl51 ='http://jkysei.space/image/10.11/1010D_277_1.jpg';picurl52 ='http://jkysei.space/image/10.11/1010D_27_1.jpg';picurl53 ='http://jkysei.space/image/10.11/1010D_285_1.jpg';picurl54 ='http://jkysei.space/image/10.11/1010D_30_1.jpg';picurl55 ='http://jkysei.space/image/10.11/1010D_51_2.jpg';picurl56 ='http://jkysei.space/image/10.11/1010D_56_1.jpg';picurl57 ='http://jkysei.space/image/10.11/1010D_60_5.jpg';picurl58 ='http://jkysei.space/image/10.11/1010D_66_1.jpg';picurl59 ='http://jkysei.space/image/10.11/1010D_68_1.jpg';picurl60 ='http://jkysei.space/image/10.11/1010D_75_1.jpg';picurl61 ='http://jkysei.space/image/10.11/1010D_93_1.jpg';
 
 function random_get(random_int){
-    if (random_int==1 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_110_1.jpg"}
-    if (random_int==2 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_12_1.jpg"}
-    if (random_int==3 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_13_1.jpg"}
-    if (random_int==4 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_148_1.jpg"}
-    if (random_int==5 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_157_1.jpg"}
-    if (random_int==6 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_162_1.jpg"}
-    if (random_int==7 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_167_1.jpg"}
-    if (random_int==8 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_179_1.jpg"}
-    if (random_int==9 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_216_1.jpg"}
-    if (random_int==10 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_221_1.jpg"}
-    if (random_int==11 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_226_1.jpg"}
-    if (random_int==12 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_239_1.jpg"}
-    if (random_int==13 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_241_1.jpg"}
-    if (random_int==14 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_245_1.jpg"}
-    if (random_int==15 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_251_1.jpg"}
-    if (random_int==16 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_30_1.jpg"}
-    if (random_int==17 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_39_1.jpg"}
-    if (random_int==18 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_46_1.jpg"}
-    if (random_int==19 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_51_1.jpg"}
-    if (random_int==20 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_56_1.jpg"}
-    if (random_int==21 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_5_1.jpg"}
-    if (random_int==22 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_63_1.jpg"}
-    if (random_int==23 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_65_1.jpg"}
-    if (random_int==24 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_73_1.jpg"}
-    if (random_int==25 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_74_1.jpg"}
-    if (random_int==26 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_76_1.jpg"}
-    if (random_int==27 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_80_1.jpg"}
-    if (random_int==28 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_91_1.jpg"}
-    if (random_int==29 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_98_1.jpg"}
-    if (random_int==30 ){
-    var pic ="http://jkysei.space/image/10.14/1011D_9_1.jpg"}
+    if (random_int==1 ){var pic =picurl1};if (random_int==2 ){var pic =picurl2};if (random_int==3 ){var pic =picurl3};if (random_int==4 ){var pic =picurl4};if (random_int==5 ){var pic =picurl5};if (random_int==6 ){var pic =picurl6};if (random_int==7 ){var pic =picurl7};if (random_int==8 ){var pic =picurl8};if (random_int==9 ){var pic =picurl9};if (random_int==10 ){var pic =picurl10};if (random_int==11 ){var pic =picurl11};if (random_int==12 ){var pic =picurl12};if (random_int==13 ){var pic =picurl13};if (random_int==14 ){var pic =picurl14};if (random_int==15 ){var pic =picurl15};if (random_int==16 ){var pic =picurl16};if (random_int==17 ){var pic =picurl17};if (random_int==18 ){var pic =picurl18};if (random_int==19 ){var pic =picurl19};if (random_int==20 ){var pic =picurl20};if (random_int==21 ){var pic =picurl21};if (random_int==22 ){var pic =picurl22};if (random_int==23 ){var pic =picurl23};if (random_int==24 ){var pic =picurl24};if (random_int==25 ){var pic =picurl25};if (random_int==26 ){var pic =picurl26};if (random_int==27 ){var pic =picurl27};if (random_int==28 ){var pic =picurl28};if (random_int==29 ){var pic =picurl29};if (random_int==30 ){var pic =picurl30};
 
-    if (random_int==31 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_106_1.jpg"}
-    if (random_int==32 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_107_1.jpg"}
-    if (random_int==33 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_114_1.jpg"}
-    if (random_int==34 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_117_1.jpg"}
-    if (random_int==35 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_11_1.jpg"}
-    if (random_int==36 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_126_1.jpg"}
-    if (random_int==37 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_144_1.jpg"}
-    if (random_int==38 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_151_1.jpg"}
-    if (random_int==39 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_153_1.jpg"}
-    if (random_int==40 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_155_1.jpg"}
-    if (random_int==41 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_162_1.jpg"}
-    if (random_int==42 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_167_1.jpg"}
-    if (random_int==43 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_173_2.jpg"}
-    if (random_int==44 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_1_17.jpg"}
-    if (random_int==45 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_1_26.jpg"}
-    if (random_int==46 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_218_1.jpg"}
-    if (random_int==47 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_228_1.jpg"}
-    if (random_int==48 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_237_1.jpg"}
-    if (random_int==49 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_258_1.jpg"}
-    if (random_int==50 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_275_1.jpg"}
-    if (random_int==51 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_277_1.jpg"}
-    if (random_int==52 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_27_1.jpg"}
-    if (random_int==53 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_285_1.jpg"}
-    if (random_int==54 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_30_1.jpg"}
-    if (random_int==55 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_51_2.jpg"}
-    if (random_int==56 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_56_1.jpg"}
-    if (random_int==57 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_60_5.jpg"}
-    if (random_int==58 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_66_1.jpg"}
-    if (random_int==59 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_68_1.jpg"}
-    if (random_int==60 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_75_1.jpg"}
-    if (random_int==61 ){
-    var pic ="http://jkysei.space/image/10.11/1010D_93_1.jpg"}
+
+    if (random_int==31 ){var pic =picurl31};if (random_int==32 ){var pic =picurl32};if (random_int==33 ){var pic =picurl33};if (random_int==34 ){var pic =picurl34};if (random_int==35 ){var pic =picurl35};if (random_int==36 ){var pic =picurl36};if (random_int==37 ){var pic =picurl37};if (random_int==38 ){var pic =picurl38};if (random_int==39 ){var pic =picurl39};if (random_int==40 ){var pic =picurl40};if (random_int==41 ){var pic =picurl41};if (random_int==42 ){var pic =picurl42};if (random_int==43 ){var pic =picurl43};if (random_int==44 ){var pic =picurl44};if (random_int==45 ){var pic =picurl45};if (random_int==46 ){var pic =picurl46};if (random_int==47 ){var pic =picurl47};if (random_int==48 ){var pic =picurl48};if (random_int==49 ){var pic =picurl49};if (random_int==50 ){var pic =picurl50};if (random_int==51 ){var pic =picurl51};if (random_int==52 ){var pic =picurl52};if (random_int==53 ){var pic =picurl53};if (random_int==54 ){var pic =picurl54};if (random_int==55 ){var pic =picurl55};if (random_int==56 ){var pic =picurl56};if (random_int==57 ){var pic =picurl57};if (random_int==58 ){var pic =picurl58};if (random_int==59 ){var pic =picurl59};if (random_int==60 ){var pic =picurl60};if (random_int==61 ){var pic =picurl61};
+
     return pic 
 }
 
