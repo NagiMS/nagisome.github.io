9b4deb1cb86a9110e9a3714e58fe31f08a353418
NagiMS <2457963787@qq.com>
2023-10-26 10:48:49 +0800

    Site updated: 2023-10-26 10:48:46

---
+++ js/layoutrp.js
@@ -0,0 +1,18 @@
+function lg_hide(){
+    $(".left-guide").css({"width":"5px"});
+    $(".left_guide_rp>li").css({"width":"5px"});
+    $(".left_guide_rp").css({"width":"5px"});
+    $(".left_guide_rp").css({"visibility":"hidden"});
+  }
+  function lg_display(){
+    $(".left-guide").css({"width":"80px"});
+    $(".left_guide_rp>li").css({"width":"80px"});
+    $(".left_guide_rp").css({"width":"150px"});
+    $(".left_guide_rp").css({"visibility":"visible"});
+  }
+   function lg_display(){
+    $(".left-guide").css({"width":"80px"});
+    $(".left_guide_rp>li").css({"width":"80px"});
+    $(".left_guide_rp").css({"width":"150px"});
+  }
+  
