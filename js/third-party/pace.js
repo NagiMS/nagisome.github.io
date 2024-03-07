32a2e9aa0979a6683ea024ea3ed5d4c0c8776030
NagiMS <2457963787@qq.com>
2024-03-07 08:59:14 +0800

    Site updated: 2024-03-07 08:59:10

--- js/third-party/pace.js
+++ js/third-party/pace.js
@@ -1,38 +1,9 @@
 /* global Pace */
-function cd0(){	
-	
-	setTimeout(cd,1000);
-}
-function cd(){
-	num -= 1;
-	setTimeout(cd0,1000);
-	if(num == 0 ){
-		$(".self-close").css("display","inline");
-	}
-  
-}
 try{
   block= $(".pace-block")
   block[0].addEventListener("animationend",() => {
-    block.css("display","none");
-    /*
-    $(".self-close").css("display","none");
-    */
-    
+    block.css("display","none"); 
   });
-  /*
-  if($(".self-close").text()!==""){
-    $(".self-close").css("display","none")
-    $(".self-close")[0].addEventListener("click",function(){
-      $(".pace-block").css("display","none");
-      $(".self-close").css("display","none");
-  
-    })
-  }
-  num=5
-  setTimeout(cd0,1000)
-  */
-  
   Pace.on("done",function(){
     block.css("animation-play-state","running");
     
